8cedb3930c7ab290975833d6361943bf820d6ae2
AlehKhvasko <alehkhvasko@yahoo.com>
2022-12-11 15:14:15 -0600

    slider

--- src/app/components/movie-item/movie-item.component.ts
+++ src/app/components/movie-item/movie-item.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from '../../models/movie';
 
 @Component({
   selector: 'movie-item',
   templateUrl: './movie-item.component.html',
   styleUrls: ['./movie-item.component.scss']
 })
-export class MovieItemComponent {
+export class MovieItemComponent implements OnInit {
+  @Input() itemData: Movie | null = null;
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
