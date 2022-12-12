c7d9df0c51be40e1ac91e88dcb18620522ee1254
AlehKhvasko <alehkhvasko@yahoo.com>
2022-12-11 20:33:34 -0600

    updates

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { MovieDto } from '../models/movie'
+import { MovieDto } from '../models/movie';
 import { switchMap, of } from 'rxjs';
 
 @Injectable({
@@ -11,11 +11,19 @@ export class MoviesService {
   apiKey: string = '8c247ea0b4b56ed2ff7d41c9a833aa77';
   constructor(private http: HttpClient) {}
 
-  getMovies(type:string = 'upcoming', count:number = 12) {
-    return this.http.get<MovieDto>(
-      `${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`
-      ).pipe(switchMap(res => {
-        return of(res.results.slice(0,count));
-      }));
+  getMovies(type: string = 'upcoming', count: number = 12) {
+    return this.http.get<MovieDto>(`${this.baseUrl}/movie/${type}?api_key=${this.apiKey}`).pipe(
+      switchMap((res) => {
+        return of(res.results.slice(0, count));
+      })
+    );
+  }
+
+  searchMovies(page: number = 1) {
+    return this.http.get<MovieDto>(`${this.baseUrl}/movie/popular?page=${page}&api_key=${this.apiKey}`).pipe(
+      switchMap((res) => {
+        return of(res.results);
+      })
+    );
   }
 }
